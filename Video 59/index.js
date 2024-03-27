17275623faa5a8d861a729fb78fbfe15c6bd2f93
Mohd Isaar <157712562+isaar1@users.noreply.github.com>
2024-03-27 09:40:16 -0400

    Update index.js

--- Video 59/index.js
+++ Video 59/index.js
@@ -13,3 +13,29 @@ This faulty calculator does following:
 It performs wrong operation 10% of the times
 
 */
+
+let random = Math.random()
+console.log(random)
+
+let a = prompt("Enter your first number: ")
+let b = prompt("Enter operation: ")
+let c = prompt("Enter your second number: ")
+
+
+obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+
+if (random > 0.1) {
+    // performs correct calculations
+    console.log(`The result is ${a} ${b} ${c}`)
+    alert(`The result is ${eval(`${a} ${b} ${c}`)}`)
+}
+else {
+    // preforms wrong calculations
+    b = obj[b]
+    alert(`The result is ${eval(`${a} ${b} ${c}`)}`)
+}
